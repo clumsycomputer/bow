597020afc561727c16722a034ebed824b0ece44b
jared mathews <clumsycomputer@gmail.com>
2024-07-02 22:53:47 +0000

    progress

--- FunctionDeclaration.ts
+++ FunctionDeclaration.ts
@@ -1,8 +1,48 @@
+ /**
+  * @example
+  * ##### addition
+  * ```
+  * full add(aaa: number, bbb: number) => number {
+  *   return aaa + bbb
+  * }
+  * 
+  * add(aaa: 2, bbb: 4)
+  * ```
+  * 
+  * @example
+  * ##### addition _(only parameters)_
+  * ```
+  * root add[aaa: number, bbb: number] => number (
+  *   return aaa + bbb 
+  * )
+  * 
+  * medium addTwo ::add[aaa: 2]
+  * 
+  * final addTwoPlusFour ::addTwo [bbb: 4]
+  * 
+  * addTwoPlusFour()
+  * ```
+  * 
+  * @example
+  * ##### addition _(parameters & arguments)_
+  * ```
+  * root add[aaa: number](bbb: number) => number (
+  *   return aaa + bbb
+  * )
+  * 
+  * final addTwo ::add [aaa: 2]
+  * 
+  * addTwo(4)
+  * ```
+  */
+ type FunctionDeclaration = FullFunctionDeclaration | RootFunctionDeclaration | MediumFunctionDeclaration | FinalFunctionDeclaration
+
+
 interface FinalFunctionDeclaration extends 
     __BasedFunctionDeclaration<'final'> {}
-
+ 
 interface MediumFunctionDeclaration extends 
-    __BasedFunctionDeclaration<'medium'>, 
+    __BasedFunctionDeclaration<'medium'>,
     __GenericFunctionDeclaration, 
     __ParameterFunctionDeclaration {}
 
@@ -16,7 +56,7 @@ interface RootFunctionDeclaration extends
     __FunctionDeclaration<'root'>, 
     __GenericFunctionDeclaration, 
     __ParameterFunctionDeclaration,
-    __RuntimeFunctionDeclaration  {}
+    __RuntimeFunctionDeclaration {}
 
 interface FullFunctionDeclaration extends 
     __FunctionDeclaration<'full'>, 
