9e9ecbae21f18a37bfdcef91f2c19ef43a38f9d6
jared mathews <clumsycomputer@gmail.com>
2024-07-03 18:33:48 +0000

    progress

---
+++ source/FunctionDeclaration.ts
@@ -0,0 +1,46 @@
+type FunctionDeclaration = FullFunctionDeclaration | RootFunctionDeclaration | MediumFunctionDeclaration | FinalFunctionDeclaration
+
+interface FinalFunctionDeclaration extends 
+    __BaseFunctionDeclaration<'final'> {}
+ 
+interface MediumFunctionDeclaration extends 
+    __BaseFunctionDeclaration<'medium'>,
+    __GenericFunctionDeclaration, 
+    __BuildFunctionDeclaration {}
+
+interface __BaseFunctionDeclaration<ThisDeclarationKind> extends 
+    __FunctionDeclaration<ThisDeclarationKind> {
+    declarationBaseName: string;
+    declarationBaseGenericInputs: unknown;
+    declarationBaseBuildInputs: unknown;
+}
+
+interface RootFunctionDeclaration extends 
+    __FunctionDeclaration<'root'>, 
+    __GenericFunctionDeclaration, 
+    __BuildFunctionDeclaration,
+    __RunFunctionDeclaration {}
+
+interface FullFunctionDeclaration extends 
+    __FunctionDeclaration<'full'>, 
+    __GenericFunctionDeclaration,
+    __RunFunctionDeclaration {}
+
+interface __FunctionDeclaration<ThisDeclarationKind> {
+    declarationKind: ThisDeclarationKind;
+    declarationName: string;
+}
+
+interface __RunFunctionDeclaration {
+    declarationRunInputs: unknown;
+    declarationRunResult: unknown;
+    declarationRunLogic: unknown;
+}
+
+interface __GenericFunctionDeclaration {
+    declarationGenericInputs: unknown;
+}
+
+interface __BuildFunctionDeclaration {
+    declarationBuildInputs: unknown;
+}
